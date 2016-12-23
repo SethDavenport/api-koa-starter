a3312d7cdde553bd61352e0f773e06db6a21e1e0
Tao, Zeyu <taozytroy@gmail.com>
2016-12-23 16:58:42 -0500

    Enable throng in production (#41)

--- src/app.js
+++ src/app.js
@@ -11,38 +11,46 @@ import { rootRouter } from './routes/root.routes';
 import { healthCheckRouter } from './routes/health-check/health-check.routes';
 import { demoRouter } from './routes/demo/demo.routes';
 
-// Entry point for all modules.
-const api = koaRouter()
-  .get('/', rootRouter.routes())
-  .use('/health', healthCheckRouter.routes())
-  .use('/demo', demoRouter.routes());
+function setUpServer(app) {
+  // Entry point for all modules.
+  const api = koaRouter()
+    .get('/', rootRouter.routes())
+    .use('/health', healthCheckRouter.routes())
+    .use('/demo', demoRouter.routes());
 
-// Top level server configuration.
-export const app = new Koa();
+  app.use(generateRequestId);
 
-app.use(generateRequestId);
+  /* istanbul ignore if */
+  if (k.REQUEST_LOGS) {
+    const morgan = require('koa-morgan');
+    const format = '[RQID=:request-id] - :remote-user' +
+      ' [:date[clf]] ":method :url HTTP/:http-version" ' +
+      ':status :res[content-length] ":referrer" ":user-agent"';
+    morgan.token('request-id', req => req.requestId);
+    app.use(morgan(format));
+  }
 
-/* istanbul ignore if */
-if (k.REQUEST_LOGS) {
-  const morgan = require('koa-morgan');
-  const format = '[RQID=:request-id] - :remote-user' +
-    ' [:date[clf]] ":method :url HTTP/:http-version" ' +
-    ':status :res[content-length] ":referrer" ":user-agent"';
-  morgan.token('request-id', req => req.requestId);
-  app.use(morgan(format));
+  app
+    .use(helmet())
+    .use(koaConvert(bodyParser()))
+    .use(errorResponder)
+    .use(api.routes())
+    .use(api.allowedMethods());
+
+  return app;
 }
 
-app
-  .use(helmet())
-  .use(koaConvert(bodyParser()))
-  .use(errorResponder)
-  .use(api.routes())
-  .use(api.allowedMethods());
-
-const PORT = process.env.PORT || 3000;
-
-/* istanbul ignore if */
-if (require.main === module) {
+function startFunction() {
+  const app = setUpServer(new Koa());
+  const PORT = process.env.PORT || 3000;
   logger.info(`Starting server on port ${PORT}`);
   app.listen(PORT);
 }
+/* istanbul ignore if */
+if (require.main === module) {
+  const throng = require('throng');
+  throng(startFunction);
+}
+
+// Top level server configuration.
+export const app = setUpServer(new Koa());
